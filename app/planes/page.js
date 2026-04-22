ccc3855a7d7f426060dafecea0c52253cbc9b2e7
unknown <selfcarebrandstore@gmail.com>
2026-04-22 00:10:16 -0600

    fix json2

--- app/planes/page.js
+++ app/planes/page.js
@@ -1,7 +1,7 @@
 'use client'
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
-import { supabase } from '@lib/supabase'
+import { supabase } from '@/lib/supabase'
 
 export default function Planes() {
  const [anual, setAnual] = useState(false)
